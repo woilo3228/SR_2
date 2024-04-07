d971abdd7d049e671bc28b4ea4561ef0dad92dff
Eth1r <45166631+woilo3228@users.noreply.github.com>
2024-04-08 02:06:36 +0700

    Program_1.cs

--- Program_1.cs
+++ Program_1.cs
@@ -1,25 +1,31 @@
-﻿// Дана последовательность дейтвительных чисел. Указать те её элементы, которые принадлежат отрезку [c,d].
+// Дана последовательность дейтвительных чисел. Указать те её элементы, которые принадлежат отрезку [c,d].
 class Program
 {
     static void Main()
     {
-        // Задаём последовательность действительных чисел
-        double[] array = { -3.5, -2.3, -2.1, -1.9, -1.1, -0.2, 0.5, 2.5, 2.8, 3.7, 5.6, 6.5, 7.8 };
+        // Выбираем размер массива
+        Console.WriteLine("Введите размер массива:");
+        int size = int.Parse(Console.ReadLine());
+        double[] array = new double[size];
 
-        // Задаём границы отрезка
-        Console.WriteLine("Введите число c");
+        // Заполняем массив
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = double.Parse(Console.ReadLine());
+        }
+
+        // Выбираем промежуток
+        Console.WriteLine("Введите число c:");
         double c = double.Parse(Console.ReadLine());
-        Console.WriteLine("Введите число c");
+        Console.WriteLine("Введите число d:");
         double d = double.Parse(Console.ReadLine());
 
-        Console.WriteLine($"Действительные числа, принадлежащие отрезку [{c}, {d}]:");
-
-        // Производим выборку
+        Console.WriteLine($"Действительные числа, принадлежащие отрезку [{c}; {d}]:");
         foreach (double number in array)
         {
             if (number >= c && number <= d)
             {
-                // Выводим действительные числа, удовлетворяющие условию
                 Console.WriteLine(number);
             }
         }
